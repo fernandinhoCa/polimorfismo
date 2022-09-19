86fbab8235054da520d63aac99927c8d90ce5d15
Fernando Capeletti <fernando.capeletti@alunos.sc.senac.br>
2022-09-19 20:22:07 -0300

    Polimorfismo

---
+++ src/polimorfismo/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author fernando.capeletti
+ */
+public class Calculadora {
+    
+    public int somar(int v1, int v2){
+        int soma = v1+v2;
+        return soma;
+    }
+}
